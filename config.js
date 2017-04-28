7b9da085848e4f64c0d1e59db6302f822595f6ca
Dustyn Tubbs <DustynTubbs@Gmail.com>
2017-04-27 22:56:09 -0500

    Adding some new providers

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.authProviders = ['twitter','reddit','facebook','github','fitbit','local']
+config.authProviders = ['twitter','reddit','facebook','github','fitbit','flickr', 'instagram', 'spotify','tumblr','youtube','local'];
 
 config.twitter = {};
   config.twitter.consumer_key    =  process.env.TWITTER_CONSUMER_KEY    || '';
@@ -22,10 +22,31 @@ config.fitbit= {};
   config.fitbit.client_id = process.env.FITBIT_ID || '';
   config.fitbit.client_secret = process.env.FITBIT_SECRET || '';
 
+config.flickr = {};
+  config.flickr.consumer_key = process.env.FLICKR_CONSUMER_KEY || '';
+  config.flickr.consumer_secret = process.env.FLICKR_CONSUMER_SECRET || '';
+
+config.instagram = {};
+  config.instagram.client_id = process.env.INSTAGRAM_ID|| '';
+  config.instagram.client_secret = process.env.INSTAGRAM_SECRET || '';
+
+config.spotify= {};
+  config.spotify.client_id = process.env.SPOTIFY_ID || '';
+  config.spotify.client_secret = process.env.SPOTIFY_SECRET || '';
+
+config.tumblr= {};
+  config.tumblr.consumer_key = process.env.TUMBLR_CONSUMER_KEY || '';
+  config.tumblr.consumer_secret = process.env.TUMBLR_CONSUMER_SECRET || '';
+
+config.youtube = {};
+  config.youtube.client_id = process.env.YOUTUBE_ID || '';
+  config.youtube.client_secret = process.env.YOUTUBE_SECRET || '';
+
 config.db = {};
   config.db.host = process.env.DB_HOST || '';
   config.db.username = process.env.DB_USERNAME || '';
   config.db.password = process.env.DB_PASSWORD || '';
   config.db.database = process.env.DB_SCHEMA || '';
+
 
 module.exports = config;
